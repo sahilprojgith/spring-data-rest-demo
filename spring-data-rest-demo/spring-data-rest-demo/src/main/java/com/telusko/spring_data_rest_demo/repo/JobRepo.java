60521b046b86aa13131fca60848ff44604f8bf86
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-07-10 22:11:19 +0530

    Add files via upload

---
+++ spring-data-rest-demo/spring-data-rest-demo/src/main/java/com/telusko/spring_data_rest_demo/repo/JobRepo.java
@@ -0,0 +1,14 @@
+package com.telusko.spring_data_rest_demo.repo;
+
+import com.telusko.spring_data_rest_demo.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost,Integer> {
+
+    //List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
+}
